efa610f5c094510a688a7e0a43783070038bd6f3
Mark Wanders <mndmwanders@gmail.com>
2019-08-31 13:56:47 +0200

    Solved challenge 9

---
+++ src/set2/Challenge9.kt
@@ -0,0 +1,22 @@
+package set2
+
+/**
+ * Implement PKCS#7 padding
+ * A block cipher transforms a fixed-sized block (usually 8 or 16 bytes) of plaintext into ciphertext. But we almost never want to transform a single block; we encrypt irregularly-sized messages.
+ *
+ * One way we account for irregularly-sized messages is by padding, creating a plaintext that is an even multiple of the blocksize. The most popular padding scheme is called PKCS#7.
+ * So: pad any block to a specific block length, by appending the number of bytes of padding to the end of the block. For instance,
+ * "YELLOW SUBMARINE"
+ * ... padded to 20 bytes would be:
+ * "YELLOW SUBMARINE\x04\x04\x04\x04"
+ */
+fun main() {
+    println(pad("YELLOW SUBMARINE", 20))
+}
+
+fun pad(input: String, paddingLength: Int): String {
+    val paddingSize = (paddingLength - (input.length % paddingLength)) // PKCS#7 padding mean the value of each added byte is the number of bytes that are added, i.e. N bytes, each of value N are added.
+    val padding = ByteArray(paddingSize) {paddingSize.toByte()}
+    val bytes = input.toByteArray().plus(padding)
+    return bytes.map { it.toChar() }.toCharArray().joinToString("")
+}
