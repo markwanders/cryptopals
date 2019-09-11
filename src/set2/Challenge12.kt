16a9b06fcf700fb91027c2bd1257da3d6e1e0272
Mark Wanders <mark.wanders@ing.com>
2019-09-11 12:38:05 +0200

    Solved challenge 14

--- src/set2/Challenge12.kt
+++ src/set2/Challenge12.kt
@@ -6,9 +6,9 @@ import java.util.*
 import kotlin.random.Random
 
 val key = Random.nextBytes(16)
-val unknownString: ByteArray = Base64.getMimeDecoder().decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg\n" +
-        "aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq\n" +
-        "dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg\n" +
+val unknownString: ByteArray = Base64.getDecoder().decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" +
+        "aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq" +
+        "dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg" +
         "YnkK")
 
 fun main() {
--- src/set2/Challenge14.kt
+++ src/set2/Challenge14.kt
@@ -10,12 +10,37 @@ import kotlin.random.Random
  *
  * Same goal: decrypt the target-bytes.
  */
-val randomPrefix = Random.nextBytes(96)
+val randomPrefix = Random.nextBytes(Random.nextInt(0, 32))
+var prefixPlusTargetLength = 0
 
 fun main() {
+    val blockSize = findBlockSize(::oracle)
     //We can find the length of the random prefix by adding increasing padding until we see a change in block repetition in the ciphertext. Then we know (padding + prefix) % keysize == 0
-    println(randomPrefix.size)
-    println(findPrefixLength())
+    val foundPrefixLength = findPrefixLength()
+    println("Random prefix length: ${randomPrefix.size}, deduced: $foundPrefixLength")
+    println(String(byteAtATimeECBDecryptionWithPrefix(::oracle, foundPrefixLength, blockSize)))
+}
+
+fun byteAtATimeECBDecryptionWithPrefix(oracle: (b: ByteArray) -> ByteArray, prefixLength: Int, blockSize: Int): ByteArray {
+    var plaintext = ByteArray(0)
+    val targetSize = prefixPlusTargetLength - prefixLength
+    (0..targetSize).forEach { _ ->
+        val paddingLength = (blockSize - ((plaintext.size + 1 + prefixLength) % blockSize)) % blockSize
+        val padding = ByteArray(paddingLength) { 'A'.toByte() }
+        val targetBlockNumber = (plaintext.size + prefixLength) / blockSize
+        val targetRange = ((targetBlockNumber * blockSize) until ((targetBlockNumber + 1) * blockSize))
+        val targetBlock = oracle(padding).slice(targetRange)
+        for (byte in 0..256) {
+            val attackerControlled = padding + plaintext + byte.toChar().toByte()
+            val encryptedBlock = oracle(attackerControlled).slice(targetRange)
+            if (encryptedBlock == targetBlock) {
+                plaintext += byte.toChar().toByte()
+                break
+            }
+        }
+    }
+
+    return plaintext
 }
 
 fun oracle(attackerControlled: ByteArray): ByteArray {
@@ -23,7 +48,7 @@ fun oracle(attackerControlled: ByteArray): ByteArray {
     return encryptAESinECBMode(plaintext, key)
 }
 
-fun toHex(bytes: ByteArray) : String {
+fun toHex(bytes: ByteArray): String {
     var output = ""
     for (b in bytes) {
         output += String.format("%02X", b)
@@ -31,19 +56,19 @@ fun toHex(bytes: ByteArray) : String {
     return output
 }
 
-fun findPrefixLength() :Int {
+fun findPrefixLength(): Int {
     var previousCipherText = ByteArray(0)
     var previousIntersect = 0
     (0..16).forEach { n ->
-        val cipherText = oracle(ByteArray(n){'A'.toByte()})
+        val cipherText = oracle(ByteArray(n) { 'A'.toByte() })
         val intersect = cipherText.toList().chunked(key.size).intersect(previousCipherText.toList().chunked(key.size).asIterable())
 //        println("${intersect.size} $n : ${toHex(cipherText)}")
-        if(intersect.isNotEmpty() && n == 1) {
+        if (intersect.isNotEmpty() && n == 1) {
             //this means prefix >= 16
             previousIntersect = intersect.size
         }
-        if(intersect.isNotEmpty() && intersect.size > previousIntersect) {
-            val prefixLength = ((intersect.size) * key.size) - (n - 1)%key.size
+        if (intersect.isNotEmpty() && intersect.size > previousIntersect) {
+            val prefixLength = ((intersect.size) * key.size) - (n - 1) % key.size
 //            println("intersects: ${intersect.size} Padding size = ${n - 1}, prefix size $prefixLength")
             return prefixLength
         }
@@ -51,4 +76,28 @@ fun findPrefixLength() :Int {
     }
     //this means prefix%16 == 0
     return previousIntersect * key.size
+}
+
+fun findBlockSize(oracle: (b: ByteArray) -> ByteArray): Int {
+    val input = ByteArray(32) { 'A'.toByte() }
+    var n = 0
+    var value = 0
+    var keySize = 0
+    while (keySize == 0) {
+        if (n == 0) {
+            value = oracle(input.copyOfRange(0, n)).size
+            n++
+        }
+        val next = oracle(input.copyOfRange(0, n + 1)).size
+        if (next > value) {
+            println("Found key with size ${next - value}")
+            keySize = next - value
+            prefixPlusTargetLength = value - n - 1
+        } else {
+            value = next
+            n++
+        }
+    }
+
+    return keySize
 }
